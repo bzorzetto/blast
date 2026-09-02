30cf931df6608386fec6b5f79a60e6c4f98132a9
Bruno Zorzetto <bruno@telepadova.tv>
2026-09-02 18:22:36 +0200

    Added cam autoswich & more

---
+++ utils/blast.js
@@ -0,0 +1,51 @@
+const EventEmitter = require('events');
+
+class Blast extends EventEmitter {
+
+    constructor() {
+
+        super();
+
+        this.debug = false;
+        this.autoswitch = false;
+        this.timer = null;
+        this.inputs = null;
+        this.input = 0;
+        this.state = false;
+    }
+
+    doCommand(command) {
+        switch(command.function) {
+            case "CAM_AUTOSWITCH":
+                this.autoswitch = !this.autoswitch;
+                this.startTimer(command.delay, command.inputs); 
+            break;
+        }
+    }
+
+    sendSwitchNowEvent(inputs) {
+        this.inputs = inputs.length;
+        this.emit("switch_now", inputs[this.input]);
+        this.input++; 
+            if (this.input >= this.inputs) {
+                this.input = 0;
+            }
+    }
+
+
+    startTimer(delay, inputs) {
+        if (this.autoswitch) {
+            this.timer = setInterval(() => this.sendSwitchNowEvent(inputs), delay);
+        } else if (!this.autoswitch) {
+            clearInterval(this.timer);
+            this.timer = null;
+            this.input = 0; 
+        }
+    }
+    
+    setState(state) {
+        this.state = state;
+    }
+}
+
+module.exports = Blast;
