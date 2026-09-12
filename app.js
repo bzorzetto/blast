309671ce2339ab102bd4a70fc0680af16e386d66
Bruno Zorzetto <bruno@telepadova.tv>
2026-09-12 09:27:51 +0200

    Removed sensible data

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ const DicaffeineClient = require('./dicaffeine/dicaffeine');
 const HomeAssistantClient = require('./ha/ha2');
 
 
-//const haApiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiMGFmMTVkYWY1ZGU0YTdhYTU1YzRhNTE4ZWUzNTkyMSIsImlhdCI6MTc4ODcxMjgxNywiZXhwIjoyMTA0MDcyODE3fQ.SF1SJDz6OuBEKtnbYpsrNjjBoQ7YXbRnsIlhuQ1tOxQ"
-const haApiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1YmY1YzdiNmI3MWU0NmRhODdkOThlODU4YzBmYjhmMiIsImlhdCI6MTc4ODg3MzA2NSwiZXhwIjoyMTA0MjMzMDY1fQ.IhlTc4CWe8yYsAJ7AeraAMmXTkGf8sn22-UsmTihfyA" 
-
 // ----------------------------//
 // Start Midi Engine           //
 // ----------------------------//
@@ -25,34 +22,8 @@ const configFile = './config.json';
 const midi = new MidiManager();
 const midiInputDevices = midi.getInputs();
 const midiOutputDevices = midi.getOutputs();
-const ha = new HomeAssistantClient('192.168.128.8', 8124, haApiToken, {protocol: 'http', debug: true});
 
-//async function test() {
-//    const states = await ha.getStates();
 
-//    states.forEach(entity => {
-
-//        console.log(
-//            entity.entity_id,
-//            entity.state
-//        );
-
-//    });
-//}
-async function test(setup) {
-
-    console.log("setup = : ", typeof(setup), setup.domain, setup.function, setup.entity);
-    
-    await ha.callService(
-            `${setup.domain}`,
-            `${setup.function}`,
-            {
-                entity_id: `${setup.entity}`
-            }
-        );
-}
-
-//test();
 
 // ----------------------------//
 // 1. Controllo configurazione //
@@ -140,6 +111,7 @@ const bose = new BoseClient(config.hosts.bose.host, config.hosts.bose.port);
 const mediaout = new MediaoutClient(config.hosts.mediaout.host, config.hosts.mediaout.portTx, config.hosts.mediaout.portRx);
 const blast = new Blast();
 const dicaffeine = [];
+const ha = new HomeAssistantClient(config.hosts.ha.host, config.hosts.ha.port, config.hosts.ha.haApiToken, {protocol: 'http', debug: true});
 
 
 
@@ -539,6 +511,37 @@ blast.on("switch_now", input => {
             //}
         });
 });
+
+// ----------------------------//
+// HA Functions                //
+// ----------------------------//
+
+//async function test() {
+//    const states = await ha.getStates();
+
+//    states.forEach(entity => {
+
+//        console.log(
+//            entity.entity_id,
+//            entity.state
+//        );
+
+//    });
+//}
+async function test(setup) {
+
+    console.log("setup = : ", typeof(setup), setup.domain, setup.function, setup.entity);
+    
+    await ha.callService(
+            `${setup.domain}`,
+            `${setup.function}`,
+            {
+                entity_id: `${setup.entity}`
+            }
+        );
+}
+//-------------------------------------
+
 
 // ----------------------------//
 // Timers                      //
